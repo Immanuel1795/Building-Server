f12161b754acbb70698b143dd28d83af9eb4c36f
Immanuel1795 <64835506+Immanuel1795@users.noreply.github.com>
2021-11-27 17:22:48 +0530

    cors

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import { MongoClient } from "mongodb";
 import dotenv from "dotenv";
 import { movieRouter } from "./routes/movies.js";
 dotenv.config();
+import cors from 'cors';
+
 
 const PORT = process.env.PORT || 9000;
 
@@ -12,6 +14,7 @@ const MONGO_URL = process.env.MONGO_URL;
 
 
 app.use(express.json());
+app.use(cors());
 
 async function createConnection() {
   const client = new MongoClient(MONGO_URL);
